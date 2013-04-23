0cf4e385e2a98214c6161a0748c2392a49c9298e
jacobtuc <jacobtuc@usc.edu>
2013-04-22 22:10:08 -0700

    Created the SelectCar window and the car select objects.

--- SelectCar.h
+++ SelectCar.h
@@ -2,16 +2,20 @@
 #define SELECTCAR_H
 
 #include <QWidget>
+#include <QGraphicsView>
+#include <QGraphicsScene>
+#include <QGraphicsTextItem>
 
 #include "MainWindow.h"
+#include "car_select.h"
 
 class MainWindow;
+class CarSelect;
 
 /**This is the widget that will be displayed to allow the user to select what car they want to use.*/
-class SelectCar : public QWidget {
+class SelectCar : public QGraphicsView {
   Q_OBJECT
 public slots:
-  void next();
 
 public:
   /**Default constructor
@@ -21,9 +25,16 @@ public:
   SelectCar(QString name, MainWindow* parent);
   /**Default destructor*/
   ~SelectCar();
+  /**This function is called from the CarSelect object when it is clicked.
+     @param car The pixmap of the car to be selected
+     @post The given pixmap is set as the user's car and the gameplay will begin.
+  */
+  void selectCar(QPixmap* car);
 
 private:
   MainWindow* parent_;
-}
+  QGraphicsScene* scene_;
+  CarSelect** carViews_;
+};
 
 #endif
